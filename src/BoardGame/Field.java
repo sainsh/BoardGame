6538807b008f5aeb4bd906e7643faf7e9885dffc
sainsh <kfsainsh@gmail.com>
2018-10-11 19:44:00 +0200

    Start of project

---
+++ src/BoardGame/Field.java
@@ -0,0 +1,4 @@
+package BoardGame;
+
+public class Field {
+}
---
+++ src/BoardGame/Main.java
@@ -0,0 +1,9 @@
+package BoardGame;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+}
